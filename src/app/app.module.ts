f52efdfe135d0fc95559bdaab614aa0179453614
Adebisi Joseph <adebisijoe@gmail.com>
2019-07-10 16:52:55 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ListPropertyComponent } from './list-property/list-property.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ViewPropertiesComponent } from './view-properties/view-properties.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    ListPropertyComponent,
+    LoginComponent,
+    RegisterComponent,
+    ViewPropertiesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+//{path:'',component:AppComponent},
+      {path:'view-properties',component:ViewPropertiesComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
